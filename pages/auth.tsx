bad296c6ed5173969050d84a960b12e63e838a77
Alex <103459735+torn80beta@users.noreply.github.com>
2023-10-03 14:47:21 +0300

    oauth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,6 +3,8 @@ import { useCallback, useState } from "react";
 import axios from "axios";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 function Auth() {
   const router = useRouter();
@@ -93,6 +95,22 @@ function Auth() {
               >
                 {variant === "login" ? "Login" : "Sign up"}
               </button>
+
+              <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                <div
+                  onClick={() => signIn("google", { callbackUrl: "/" })}
+                  className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                >
+                  <FcGoogle size={30} />
+                </div>
+                <div
+                  onClick={() => signIn("github", { callbackUrl: "/" })}
+                  className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                >
+                  <FaGithub size={30} />
+                </div>
+              </div>
+
               <p className="text-neutral-500 mt-12">
                 {variant === "login"
                   ? "First time using Netflix?"
