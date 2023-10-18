d7df2d06047f2139b4c8310a6910c641f59a65de
Alex <hotbox80@gmail.com>
2023-10-18 23:56:38 +0300

    prisma fix, profile page

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,7 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = global.prismadb || new PrismaClient();
+// const client = global.prismadb || new PrismaClient();
+let client;
 
-if (process.env.NODE_ENV === "production") global.prismadb = client;
+// if (process.env.NODE_ENV === "production") global.prismadb = client;
+
+if (process.env.NODE_ENV === "production") {
+  client = new PrismaClient();
+} else {
+  if (!global.prismadb) {
+    global.prismadb = new PrismaClient();
+  }
+
+  client = global.prismadb;
+}
 
 export default client;
