7983f1595f6e3a5872cd35b6f2f2e77995429f77
Alex <103459735+torn80beta@users.noreply.github.com>
2023-11-15 22:56:06 +0200

    prisma fix

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,18 @@
 import type { PrismaClient } from "@prisma/client";
 // import type { MongoClient } from "mongodb";
 
+/*  */
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
+
+// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+/*  */
+
 declare global {
   namespace globalThis {
-    var prismadb: PrismaClient;
+    // var prismadb: PrismaClientSingleton | undefined;
+    var prisma: PrismaClient;
   }
 }
--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,18 +1,39 @@
 import { PrismaClient } from "@prisma/client";
 
-// const client = global.prismadb || new PrismaClient();
-let client;
+// // const client = global.prismadb || new PrismaClient();
 
-// if (process.env.NODE_ENV === "production") global.prismadb = client;
+// // if (process.env.NODE_ENV === "production") global.prismadb = client;
 
-if (process.env.NODE_ENV === "production") {
-  client = new PrismaClient();
-} else {
-  if (!global.prismadb) {
-    global.prismadb = new PrismaClient();
-  }
+// let client;
 
-  client = global.prismadb;
-}
+/* prisma client type fix  */
 
-export default client;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+/*  */
+
+// if (process.env.NODE_ENV === "production") {
+//   client = new PrismaClient();
+// } else {
+//   if (!global.prismadb) {
+//     global.prismadb = new PrismaClient();
+//   }
+
+//   client = global.prismadb;
+// }
+
+// export default client;
